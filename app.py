54e03158878b3aa4955dccab873c4eb318a74189
Fabio Araujo <araujosilvafabio@hotmail.com>
2023-08-11 10:46:24 -0300

    adiciona else para o if

--- app.py
+++ app.py
@@ -2,4 +2,6 @@ nome = input('\nDigite seu nome: ')
 idade = int(input('\nDigite sua idade: '))
 
 if idade >= 18:
-    print(f'Ola {nome}, você tem {idade} anos e é maio de idade')
+    print(f'Ola {nome}, você tem {idade} anos e é maior de idade')
+else:
+    print(f'Ola {nome}, você tem {idade} anos e é menor de idade')
